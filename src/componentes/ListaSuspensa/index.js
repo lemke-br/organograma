21eec20f7960447615ca6dbc7e71ac40b5c8b401
Guilherme Lemke <glemkee@gmail.com>
2024-06-17 13:38:25 -0300

    final

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -9,7 +9,7 @@ const ListaSuspensa = (props) => {
             <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.valor}>
                 <option value=""></option>
                 {props.itens.map(item => {
-                    return <option >{item}</option>
+                    return <option key={item} >{item}</option>
                 })}
             </select>
         </div >
